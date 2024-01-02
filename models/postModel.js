e3ac51008006e884bdbab1bf700da6ca80713451
Carlos Pires <extraterra1@gmail.com>
2024-01-02 11:59:01 +0000

    dev: post model

---
+++ models/postModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: 'You must provide a title for your blog post'
+  },
+  content: {
+    type: String,
+    trim: true,
+    required: 'You must provide content for your blog post'
+  },
+  author: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: 'You must provide a valid user id'
+  },
+  added: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Post', postSchema);
