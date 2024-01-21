5601b4a12556899577317b95e6efa47f8b7e3311
Carlos Pires <extraterra1@gmail.com>
2024-01-21 20:42:31 +0000

    dev: comment model

---
+++ models/CommentModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    trim: true,
+    required: 'Your comment cannot be empty'
+  },
+  author: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: 'You must provide a valid user id'
+  },
+  post: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Post',
+    required: 'You must provide a valid post id'
+  },
+  added: {
+    type: Date,
+    default: Date.now
+  },
+  likes: {
+    type: Number,
+    trim: true,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model('Post', commentSchema);
