5df533df9a1050595b6bc84fea71a766eb980828
Carlos Pires <extraterra1@gmail.com>
2024-01-22 12:19:09 +0000

    dev: get comments by user route

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -11,3 +11,12 @@ exports.getComments = asyncHandler(async (req, res) => {
 
   return res.json({ comments });
 });
+
+exports.getCommentsByUser = asyncHandler(async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) return res.status(401).json({ err: { message: 'Invalid User' } });
+
+  const posts = await Comment.find({ author: req.params.id }).populate('author');
+  if (posts.length === 0) return res.json({ posts, msg: 'No comments by this author' });
+
+  return res.json({ posts });
+});
